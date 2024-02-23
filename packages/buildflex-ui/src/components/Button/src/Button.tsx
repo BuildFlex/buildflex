642fa37168cdd4a86261913678796717df5a9916
Ichiro Kentaro <ichiro.kentaro.2605@gmail.com>
2024-02-23 15:37:54 +0700

    Add builflex server

--- packages/buildflex-ui/src/components/Button/src/Button.tsx
+++ packages/buildflex-ui/src/components/Button/src/Button.tsx
@@ -1,6 +1,15 @@
 import React, { forwardRef } from "react";
+import styled from 'styled-components'
 import { Button as AntButton } from 'antd';
+
 const _Button =  () => (
-    <AntButton type="primary">Primary Button</AntButton>
+    <StyledButton>Click me</StyledButton>
 );
+const StyledButton = styled(AntButton)`
+  background-color: #4DABD1;
+  &:hover {
+    background-color: #E1E8ED;
+  }
+  color: white
+`
 export const Button = forwardRef(_Button);
