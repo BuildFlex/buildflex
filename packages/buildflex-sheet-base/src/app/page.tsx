b3d3fd7c69f55ddd2b02c77a532bf2df88025a1d
Ichiro Kentaro <ichiro.kentaro.2605@gmail.com>
2024-02-23 17:22:29 +0700

    add shared ui and sheet base

---
+++ packages/buildflex-sheet-base/src/app/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import styled from 'styled-components';
+import {Button} from "@shared-ui";
+
+const StyledPage = styled.div`
+  .page {
+  }
+`;
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.styled-components file.
+   */
+  return (
+    <StyledPage>
+      <Button />
+    </StyledPage>
+  );
+}
